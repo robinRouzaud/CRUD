03cc9e65084d7be95e1ce08e4bba6ed1a8b9027c
inti0397 <inti0397@DESKTOP-B0V1LT6.home>
2018-01-08 16:39:16 +0100

    commit 3

---
+++ CRUD/src/main/java/fr/crud/rest/FournisseurResource.java
@@ -0,0 +1,37 @@
+package fr.crud.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.crud.entity.Fournisseur;
+import fr.crud.service.IFournisseurService;
+
+@RestController
+@RequestMapping("/api")
+public class FournisseurResource {
+
+	@Autowired
+	private IFournisseurService fournisseurService;
+	
+	@RequestMapping(value = "/fournisseurs", method = RequestMethod.POST, produces="application.json")
+	public Fournisseur ajouterFournisseur(@RequestBody Fournisseur fournisseur) {
+		Fournisseur f = fournisseurService.createFournisseur(fournisseur);
+		return f;
+	}
+	
+	@RequestMapping(value="/fournisseur", method=RequestMethod.GET)
+	public List<Fournisseur> getFournisseur() {
+		return fournisseurService.getAllFournisseur();
+	}
+	
+	@RequestMapping(value="/fournisseur/{id}", method=RequestMethod.DELETE)
+	public void deleteFournisseur(@PathVariable("id") Long id) {
+		fournisseurService.deleteFournisseur(id);
+	}
+}
